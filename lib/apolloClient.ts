f9fdbe6711cdb0121ebb28dfed4d69a4a1f4f71d
ofirrabi99 <34262665+ofirrabi99@users.noreply.github.com>
2022-12-26 23:15:48 +0200

    Update apolloClient.ts

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_URL,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({ addTypename: false }),
 });
 
 export default client;

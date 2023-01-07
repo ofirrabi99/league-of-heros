ff4ab0f01a5bd49a20bc7c913f760e55fc1cc904
ofirrabi99 <34262665+ofirrabi99@users.noreply.github.com>
2023-01-07 22:50:43 +0200

    Added chakraUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,20 +7,24 @@ import Layout from "../components/layout/Layout";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config as fontAwesomeConfig } from "@fortawesome/fontawesome-svg-core";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../lib/chakraUi";
 
 fontAwesomeConfig.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <main className={`${inter.className} wrapper`}>
-        <ApolloProvider client={client}>
-          <UserProvider>
-            <Component {...pageProps} />
-          </UserProvider>
-        </ApolloProvider>
-      </main>
-    </Layout>
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <main className={`${inter.className} wrapper`}>
+          <ApolloProvider client={client}>
+            <UserProvider>
+              <Component {...pageProps} />
+            </UserProvider>
+          </ApolloProvider>
+        </main>
+      </Layout>
+    </ChakraProvider>
   );
 }

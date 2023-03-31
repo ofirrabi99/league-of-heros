f1437927e80fdf5a00e47a936fcd7b3ea5a14a7b
ofirrabi99 <34262665+ofirrabi99@users.noreply.github.com>
2023-04-01 01:42:54 +0300

    Update index.ts

--- src/pages/api/graphql/index.ts
+++ src/pages/api/graphql/index.ts
@@ -9,6 +9,7 @@ import { TeamResolver } from "./features/team/team.resolver";
 import { GameResolver } from "./features/games/game.resolver";
 
 async function dbConnect() {
+  console.log("START DB CONNECTION...");
   const MONGODB_URI = process.env.MONGODB_URI;
   if (!MONGODB_URI) {
     throw new Error("Please define the MONGODB_URI environment variable");
@@ -19,6 +20,7 @@ async function dbConnect() {
 
   if (mongoose.connections[0].readyState) return;
   await mongoose.connect(MONGODB_URI);
+  console.log("CREATED NEW DB CONNECTION!");
 }
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {

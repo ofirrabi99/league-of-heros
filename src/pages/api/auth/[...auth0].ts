474f6843c6b48b38cdfe4d3929b93e8477cf1afc
ofirrabi99 <34262665+ofirrabi99@users.noreply.github.com>
2023-04-25 18:36:30 +0300

    Update [...auth0].ts

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -3,7 +3,8 @@ import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 export default handleAuth({
   async login(req, res) {
     await handleLogin(req, res, {
-      getLoginState: (r, o) => {
+      // Fix login bug - missing subpath of custom locale
+      getLoginState: (r) => {
         const to = r.query.returnTo;
         const locale = r.cookies["NEXT_LOCALE"];
         return { returnTo: `/${locale}${to}` };

b55c1473a696799c7c79a1442d0ba12f20bcf6d5
ofirrabi99 <34262665+ofirrabi99@users.noreply.github.com>
2023-04-25 18:36:00 +0300

    Update [...auth0].ts

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,3 +1,13 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+export default handleAuth({
+  async login(req, res) {
+    await handleLogin(req, res, {
+      getLoginState: (r, o) => {
+        const to = r.query.returnTo;
+        const locale = r.cookies["NEXT_LOCALE"];
+        return { returnTo: `/${locale}${to}` };
+      },
+    });
+  },
+});

ce7a265a30f5612ab1ec221ff9199b09987def16
ofirrabi99 <34262665+ofirrabi99@users.noreply.github.com>
2023-05-01 17:12:12 +0300

    Update team.service.test.ts

--- src/pages/api/graphql/features/team/team.service.test.ts
+++ src/pages/api/graphql/features/team/team.service.test.ts
@@ -1,28 +1,32 @@
 import { TeamModel } from "./team.model";
 import TeamService from "./team.service";
-import PlayerService from "../player/player.service";
+import Container from "typedi";
 
 describe("team service", () => {
-  const service = new TeamService(new PlayerService());
   const team = { name: "name", imageUrl: "image", players: [] };
+  let teamService: TeamService;
+
+  beforeEach(() => {
+    teamService = Container.get(TeamService);
+  });
 
   describe("getAll", () => {
     it("should return empty array without teams", async () => {
-      const teams = await service.getAll();
+      const teams = await teamService.getAll();
       expect(Array.isArray(teams)).toBe(true);
       expect(teams.length).toEqual(0);
     });
 
     it("should return array with all created teams", async () => {
       await TeamModel.create(team);
-      const teams = await service.getAll();
+      const teams = await teamService.getAll();
       expect(teams.length).toEqual(1);
     });
   });
 
   describe("setTeam", () => {
     it("should create new team", async () => {
-      const newTeam = await service.setTeam(team);
+      const newTeam = await teamService.setTeam(team);
       expect(newTeam).toStrictEqual(
         expect.objectContaining({ name: team.name, imageUrl: team.imageUrl })
       );
@@ -34,7 +38,7 @@ describe("team service", () => {
 
     it("should fail on new team with bad inputs", async () => {
       await expect(
-        service.setTeam({ name: "", imageUrl: "", players: [] })
+        teamService.setTeam({ name: "", imageUrl: "", players: [] })
       ).rejects.toThrowError();
       const teams = await TeamModel.find();
       expect(teams.length).toEqual(0);
@@ -43,7 +47,7 @@ describe("team service", () => {
     it("should fail on existing team with bad inputs", async () => {
       const newTeam = await TeamModel.create(team);
       await expect(
-        service.setTeam({
+        teamService.setTeam({
           _id: newTeam._id,
           imageUrl: "",
           name: "",
@@ -58,7 +62,7 @@ describe("team service", () => {
 
     it("change existing team", async () => {
       const newTeam = await TeamModel.create(team);
-      const changedTeam = await service.setTeam({
+      const changedTeam = await teamService.setTeam({
         _id: newTeam._id,
         name: "aaaaa",
         imageUrl: "dddd",
@@ -73,8 +77,8 @@ describe("team service", () => {
   describe("deleteTeam", () => {
     it("should return null when delete non existing team", async () => {
       const newTeam = await TeamModel.create(team);
-      await service.deleteTeam(newTeam._id);
-      const deletedTeam = await service.deleteTeam(newTeam._id);
+      await teamService.deleteTeam(newTeam._id);
+      const deletedTeam = await teamService.deleteTeam(newTeam._id);
       expect(deletedTeam).toStrictEqual(null);
     });
 
@@ -83,7 +87,7 @@ describe("team service", () => {
       let teams = await TeamModel.find({});
       expect(teams.length).toEqual(1);
 
-      const deletedTeam = await service.deleteTeam(newTeam._id);
+      const deletedTeam = await teamService.deleteTeam(newTeam._id);
       teams = await TeamModel.find({});
       expect(teams.length).toEqual(0);
       expect(deletedTeam).toStrictEqual(
